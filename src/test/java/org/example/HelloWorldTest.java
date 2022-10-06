87070ebcbd3cfe5e3bf28b0d48c8beb717993663
Anshuman Singh <41586858+Anshuman-37@users.noreply.github.com>
2022-10-06 11:30:43 +0100

    First Commit

---
+++ src/test/java/org/example/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldTest {
+    @Test
+    public void baseCase(){
+        assertEquals(true,true);
+    }
+}
